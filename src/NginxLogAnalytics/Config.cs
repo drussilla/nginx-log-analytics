cd048ab1ccfc01d1c46e04b662421723e437e573
Ivan Derevianko <drussilla7@gmail.com>
2019-12-11 11:18:16 -0800

    Show only actula content

---
+++ src/NginxLogAnalytics/Config.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Text.Json;
+
+namespace NginxLogAnalytics
+{
+    public class Config
+    {
+        public string LogFilePath { get; set; }
+        public string CrawlerUserAgentsFilePath { get; set; }
+        public string ExcludeContentFilePath { get; set; }
+
+        public static Config Load(string path)
+        {
+            if (!File.Exists(path))
+            {
+                throw new FileNotFoundException($"Config doesn't exists at {path}.");
+            }
+
+            return JsonSerializer.Deserialize<Config>(File.ReadAllText(path));
+        }
+    }
+}
--- src/NginxLogAnalytics/Program.cs
+++ src/NginxLogAnalytics/Program.cs
@@ -1,9 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading;
 
@@ -13,40 +13,33 @@ namespace NginxLogAnalytics
     {
         static void Main(string[] args)
         {
-            if (args.Length != 1)
-            {
-                Console.WriteLine("Please specify access.log file path");
-                return;
-            }
-
-            if (!File.Exists(args[0]))
-            {
-                Console.WriteLine($"Specify file {args[0]} doesn't exist.");
-                return;
-            }
-
             Console.OutputEncoding = Encoding.UTF8;
-            var parser = new LogParser(args[0]);
+            var config = Config.Load("config.json");
+
+            _crawlerUserAgents = File.ReadAllLines(config.CrawlerUserAgentsFilePath);
+            
+            var parser = new LogParser(config.LogFilePath);
             var items = parser.ParseAsync(CancellationToken.None).Result;
 
-            var filteredItems = items
-                .Where(x => !IsCrawler(x) && IsPages(x) && x.ResponseCode != HttpStatusCode.NotFound).ToList();
+            var contentNotCrawlers = items
+                .Where(x => !IsCrawler(x) && IsPages(x) && (int)x.ResponseCode < 400 && x.ResponseCode != HttpStatusCode.Moved).ToList();
 
             Console.WriteLine($"Total: {items.Count}");
-            Console.WriteLine($"Filtered: {filteredItems.Count}");
+            Console.WriteLine($"Filtered: {contentNotCrawlers.Count}");
 
-            var result = filteredItems.GroupBy(x => x.NormalizedRequestUrl)
-                .Select(x => new { Url = x.Key, Count = x.Count(), LogItems = x})
+            var result = contentNotCrawlers.GroupBy(x => x.NormalizedRequestUrl)
+                .Select(x => new { Url = x.Key, Count = x.Count(), OkCOunt = x.Count(y => (int)y.ResponseCode >= 200 && (int)y.ResponseCode < 300), LogItems = x})
+                .Where(x => x.Count > 1)
                 .OrderByDescending(x => x.Count);
 
             foreach (var item in result)
             {
-                Console.WriteLine(item.Count + "\t\t" + item.Url);
-                    foreach (var userAgent in item.LogItems.Select(x => x.UserAgent).Distinct())
-                    {
-                        Console.WriteLine($"\t{userAgent}");
-                    }
+                Console.WriteLine($"{item.Count} - {item.OkCOunt} - {item.Url}");
             }
+
+            //var notFoundItems = items.Where(x => x.ResponseCode == HttpStatusCode.NotFound).ToList();
+
+            //var crawlers = items.Where(IsCrawler).ToList();
 
         }
 
@@ -57,17 +50,33 @@ namespace NginxLogAnalytics
                    !logItem.NormalizedRequestUrl.StartsWith("/favicon") &&
                    !logItem.NormalizedRequestUrl.StartsWith("/site.webmanifest") &&
                    !logItem.NormalizedRequestUrl.StartsWith("/comment.aspx") &&
+                   !logItem.NormalizedRequestUrl.StartsWith("/comment-submitted") &&
                    !logItem.NormalizedRequestUrl.StartsWith("/fonts/") &&
+                   !logItem.NormalizedRequestUrl.StartsWith("/robots.txt") &&
                    !logItem.NormalizedRequestUrl.EndsWith(".png") &&
                    !logItem.NormalizedRequestUrl.EndsWith(".svg") &&
                    !logItem.NormalizedRequestUrl.EndsWith(".jpg")
                 ;
         }
 
+        private static string[] _crawlerUserAgents;
+
         private static bool IsCrawler(LogItem logItem)
         {
-            return logItem.UserAgent.Contains("bot", StringComparison.OrdinalIgnoreCase) ||
-                   logItem.UserAgent.Contains("feed", StringComparison.OrdinalIgnoreCase);
+            if (logItem.UserAgent == "-")
+            {
+                return true;
+            }
+
+            for (int i = 0; i < _crawlerUserAgents.Length; i++)
+            {
+                if (logItem.UserAgent.Contains(_crawlerUserAgents[i], StringComparison.InvariantCultureIgnoreCase))
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
